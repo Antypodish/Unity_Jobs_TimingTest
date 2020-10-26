5025572083683f4fbe6f72f1316637fe3cf57def
Dobromił Duda <dobduda@gmail.com>
2020-10-26 17:49:27 +0100

    Timing Test Systems
    Adding:
    JobsBranchingTimingTestSystem.cs
    JobsHashmapTimingTestSystem.cs

---
+++ JobsBranchingTimingTestSystem.cs
@@ -0,0 +1,238 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+using Unity.Collections;
+using Unity.Entities ;
+using Unity.Jobs;
+
+// using System.Diagnostics;
+
+namespace Antypodish.ECS.Tests
+{
+
+    public class JobsBranchingTimingTestSystem : SystemBase
+    {
+
+        System.Diagnostics.Stopwatch stopwatch ;
+
+        protected override void OnCreate ()
+        {
+            stopwatch = new System.Diagnostics.Stopwatch () ;
+        }
+
+        
+        protected override void OnUpdate ( )
+        {
+
+            int i_len = 1000000 ;
+            NativeArray <int> na_i = new NativeArray<int> (3, Allocator.TempJob )  ;
+        
+
+
+            int a = 3; int b = 6 ; int z = 0 ;
+ 
+    Debug.LogWarning ( "Main thread no Burst" ) ;
+            
+            stopwatch.Restart () ;
+
+            for ( int i = 0; i < i_len; i ++ )
+            {
+                z = i > b ? i : b ;
+                // z = a > b ? a : b ;
+            }
+
+            // 5ms
+    Debug.Log ( "branching ? " + stopwatch.ElapsedMilliseconds ) ;
+        
+            stopwatch.Restart () ;
+
+            for ( int i = 0; i < i_len; i ++ )
+            {
+                z = b ^ ((i ^ b) & -(i << b)) ;
+                // z = b ^ ((a ^ b) & -(a >> b)) ;
+            }
+
+            // 3ms
+    Debug.Log ( "no branching " + stopwatch.ElapsedMilliseconds ) ;
+        
+
+            stopwatch.Restart () ;
+
+            for ( int i = 0; i < i_len; i ++ )
+            {
+                z = i ;
+                // z = a ;
+                if ( i <= b ) z = b ;
+                // if ( a <= b ) z = b ;
+            }
+
+            // 6ms
+    Debug.Log ( "no branching if " + stopwatch.ElapsedMilliseconds ) ;
+        
+            stopwatch.Restart () ;
+
+            for ( int i = 0; i < i_len; i ++ )
+            {
+                if ( a > b ) { z = a ; } else { z = b ; } ;
+            }
+
+            // 8ms
+    Debug.Log ( "if else " + stopwatch.ElapsedMilliseconds ) ;
+
+
+    Debug.LogWarning ( "Parallel For Jobs Burst" ) ;
+
+             na_i [0] = 1 ;
+             na_i [1] = 2 ;
+            // JobHandle dep ;
+
+            stopwatch.Restart () ;
+
+            Dependency = new BranchingQuestion ( )
+            {
+                na_i = na_i,
+            }.Schedule ( i_len, 256, Dependency ) ;
+            Dependency.Complete () ;
+
+            // 10 ms
+    Debug.Log ( "burst branching ? " + stopwatch.ElapsedMilliseconds ) ;
+
+            stopwatch.Restart () ;
+
+            Dependency = new NoBranching ( )
+            {
+                na_i = na_i,
+            }.Schedule ( i_len, 256, Dependency ) ;
+            Dependency.Complete () ;
+
+            // 22 ms
+    Debug.Log ( "burst no branching " + stopwatch.ElapsedMilliseconds ) ;
+        
+            stopwatch.Restart () ;
+
+            Dependency = new NoBranchingIf ( )
+            {
+                na_i = na_i,
+            }.Schedule ( i_len, 256, Dependency ) ;
+            Dependency.Complete () ;
+
+            // 10 ms
+    Debug.Log ( "burst no branching if " + stopwatch.ElapsedMilliseconds ) ;
+        
+            stopwatch.Restart () ;
+
+            Dependency = new BranchingIfElse ( )
+            {
+                na_i = na_i,
+            }.Schedule ( i_len, 256, Dependency ) ;
+            Dependency.Complete () ;
+
+            // 10 ms
+    Debug.Log ( "burst branching if else " + stopwatch.ElapsedMilliseconds ) ;
+
+            stopwatch.Stop () ;
+
+            na_i.Dispose () ;
+
+            Debug.LogError ( "Stop" ) ;
+
+        }
+
+
+
+        [BurstCompatible]
+        struct Sort : IJob
+        {
+            public NativeArray <int> withDuplicates ;
+            public void Execute ()
+            {
+                withDuplicates.Sort () ;
+            }
+        }
+    
+        [BurstCompatible]
+        struct Unique : IJob
+        {
+            public NativeArray <int> i ;
+            public NativeArray <int> withDuplicates ;
+            public void Execute ()
+            {
+                i [0] = withDuplicates.Unique () ;
+            }
+        }
+
+    
+
+
+
+        [BurstCompatible]
+        struct BranchingQuestion : IJobParallelFor
+        {
+            [ReadOnly]
+            public NativeArray <int> na_i ;
+            public void Execute ( int i )
+            {
+                // for ( int i = 0; i < 1000000; i ++ )
+                //{
+                int a = i > na_i [1] ? i : na_i [1] ;
+    //                 na_i [2] = i > na_i [1] ? i : na_i [1] ;
+                //}
+            }
+        }
+    
+        [BurstCompatible]
+        struct NoBranching : IJobParallelFor
+        {
+            [ReadOnly]
+            public NativeArray <int> na_i ;
+            public void Execute ( int i )
+            {
+                //for ( int i = 0; i < 1000000; i ++ )
+                //{
+
+                int a = na_i [1] ^ ((i ^ na_i [1]) & -(i << na_i [1])) ;
+                    // na_i [2] = na_i [1] ^ ((i ^ na_i [1]) & -(i >> na_i [1])) ;
+                //}
+            }
+        }
+
+    
+        [BurstCompatible]
+        struct NoBranchingIf : IJobParallelFor
+        {
+            [ReadOnly]
+            public NativeArray <int> na_i ;
+            public void Execute ( int i )
+            {
+                //for ( int i = 0; i < 1000000; i ++ )
+                //{
+                int a = i ;
+                if ( i <= na_i [1] ) a = na_i [1] ;
+                    // na_i [2] = i ;
+                    // if ( i <= na_i [1] ) na_i [2] = na_i [1] ;
+                //}
+            }
+        }
+    
+
+    
+        [BurstCompatible]
+        struct BranchingIfElse : IJobParallelFor
+        {
+            [ReadOnly]
+            public NativeArray <int> na_i ;
+            public void Execute ( int i )
+            {
+                //for ( int i = 0; i < 1000000; i ++ )
+                //{
+                int a ;
+                if ( i > na_i [1] ) { a = i ; } else { a = na_i [1] ; }
+                    // if ( i > na_i [1] ) { na_i [2] = i ; } else { na_i [2] = na_i [1] ; }
+                //}
+            }
+        }
+
+    }
+
+}
---
+++ JobsHashmapTimingTestSystem.cs
@@ -0,0 +1,121 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+using Unity.Entities ;
+using Unity.Collections;
+using Unity.Jobs;
+
+
+namespace Antypodish.ECS.Tests
+{
+
+    public class JobsHashmapTimingTestSystem : SystemBase
+    {
+
+        System.Diagnostics.Stopwatch stopwatch ;
+
+        protected override void OnCreate ()
+        {
+            stopwatch = new System.Diagnostics.Stopwatch () ;
+            stopwatch.Restart () ;
+            stopwatch.Start () ;
+
+
+            NativeMultiHashMap <int,int> dic = new NativeMultiHashMap <int, int> ( 100000, Allocator.Temp ) ;
+            NativeMultiHashMap <int,int> dic2 = new NativeMultiHashMap <int, int> ( 100000, Allocator.Temp ) ;
+
+            for ( int i = 0; i < 100000; i ++ )
+            {
+                var v2 = new Vector2Int ( Random.Range ( 1001, -1000 ), Random.Range ( -1000, 1000 ) ) ;
+                dic.Add ( v2.x, v2.y ) ;
+                dic2.Add ( v2.x, v2.y ) ;
+    // Debug.Log ( "v2: " + v2 ) ;
+            }
+        
+
+    Debug.Log ( "generate " + stopwatch.ElapsedMilliseconds ) ;
+
+            // NativeMultiHashMap <int,int> dic = nmhm_newBornEntitiesPerTile ;
+
+            var withDuplicates = dic.GetKeyArray ( Allocator.TempJob ) ;
+            var withDuplicates2 = dic2.GetKeyArray ( Allocator.TempJob ) ;
+        
+    Debug.Log ( "get keys " + stopwatch.ElapsedMilliseconds ) ;
+            /*
+            for ( int i = 0; i < 1000; i ++ )
+            {
+                Debug.Log ( "A: " + withDuplicates [i] ) ;
+            }
+            */
+
+        
+            withDuplicates.Sort();
+    Debug.Log ( "e1 sort A " + stopwatch.ElapsedMilliseconds ) ;
+            int uniqueCount = withDuplicates.Unique () ;
+        
+    Debug.Log ( "e2 unique A " + stopwatch.ElapsedMilliseconds ) ;
+            new Sort ( )
+            {
+                withDuplicates = withDuplicates2
+            }.Schedule ().Complete () ;
+        
+    Debug.Log ( "e1 sort B " + stopwatch.ElapsedMilliseconds ) ;
+        
+            NativeArray <int> na_i = new NativeArray<int> (3, Allocator.TempJob )  ;
+            new Unique ( )
+            {
+                i = na_i,
+                withDuplicates = withDuplicates2
+            }.Schedule ().Complete () ;
+        
+            // uniqueCount = withDuplicates2.Unique () ;
+        
+    Debug.Log ( "e2 unique B " + stopwatch.ElapsedMilliseconds ) ;
+
+    Debug.Log ( "uniqueCount " + uniqueCount ) ;
+       
+        
+            for ( int i = 0; i < uniqueCount; i ++ )
+            {
+                Debug.Log ( "B: " + withDuplicates [i] ) ;
+            }
+        
+            withDuplicates2.Dispose () ;
+            dic2.Dispose () ;
+            withDuplicates.Dispose () ;
+            dic.Dispose () ;
+
+        }
+
+        [BurstCompatible]
+        struct Sort : IJob
+        {
+            public NativeArray <int> withDuplicates ;
+            public void Execute ()
+            {
+                withDuplicates.Sort () ;
+            }
+        }
+    
+        [BurstCompatible]
+        struct Unique : IJob
+        {
+            public NativeArray <int> i ;
+            public NativeArray <int> withDuplicates ;
+            public void Execute ()
+            {
+                i [0] = withDuplicates.Unique () ;
+            }
+        }
+
+    
+
+
+        protected override void OnUpdate ( )
+        {
+        }
+
+    }
+
+}
