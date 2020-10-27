9b29eca737cd133d77333fe2fa55fc22cee26713
Dobromił Duda <dobduda@gmail.com>
2020-10-27 01:41:02 +0100

    Changes to timings - added hash map.
    Printed out times to console, now gives relative values, to be clear to read.
    Added NativeHashMap, to store unique values.

--- JobsHashmapTimingTestSystem.cs
+++ JobsHashmapTimingTestSystem.cs
@@ -1,10 +1,11 @@
-﻿using System.Collections;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-using Unity.Entities ;
 using Unity.Collections;
-using Unity.Jobs;
+using Unity.Entities ;
+using Unity.Burst ;
+using Unity.Jobs ;
 
 
 namespace Antypodish.ECS.Tests
@@ -21,13 +22,115 @@ namespace Antypodish.ECS.Tests
             stopwatch.Restart () ;
             stopwatch.Start () ;
 
+            int i_len = 1000000 ;
 
-            NativeMultiHashMap <int,int> dic = new NativeMultiHashMap <int, int> ( 100000, Allocator.Temp ) ;
-            NativeMultiHashMap <int,int> dic2 = new NativeMultiHashMap <int, int> ( 100000, Allocator.Temp ) ;
+            NativeMultiHashMap <int,int> dic = new NativeMultiHashMap <int, int> ( i_len, Allocator.Temp ) ;
+            NativeMultiHashMap <int,int> dic2 = new NativeMultiHashMap <int, int> ( i_len, Allocator.Temp ) ;
 
-            for ( int i = 0; i < 100000; i ++ )
+            NativeHashMap <int, int> dic3 = new NativeHashMap <int, int> ( i_len, Allocator.Temp ) ;
+            for ( int i = 0; i < i_len; i ++ )
             {
-                var v2 = new Vector2Int ( Random.Range ( 1001, -1000 ), Random.Range ( -1000, 1000 ) ) ;
+                var v2 = new Vector2Int ( Random.Range ( 10, -10 ), Random.Range ( -1000, 1000 ) ) ;
+                dic.Add ( v2.x, v2.y ) ;
+                dic2.Add ( v2.x, v2.y ) ;
+                dic3.TryAdd ( v2.x, v2.y ) ;
+    // Debug.Log ( "v2: " + v2 ) ;
+            }
+            
+    Debug.Log ( "populate hashmaps " + stopwatch.ElapsedMilliseconds + "ms" ) ;
+            stopwatch.Restart () ;
+            // NativeMultiHashMap <int,int> dic = nmhm_newBornEntitiesPerTile ;
+
+            var withDuplicates = dic.GetKeyArray ( Allocator.Temp ) ;
+            
+    Debug.Log ( "multi hashmap get keys A " + stopwatch.ElapsedMilliseconds + "ms" ) ;
+            stopwatch.Restart () ;
+            var withDuplicates2 = dic2.GetKeyArray ( Allocator.TempJob ) ;
+            
+    Debug.Log ( "multi hashmap get keys B " + stopwatch.ElapsedMilliseconds + "ms" ) ;
+            stopwatch.Restart () ;
+
+            var noDuplicates = dic3.GetKeyArray ( Allocator.Temp ) ;
+    Debug.Log ( "hashmap get keys " + stopwatch.ElapsedMilliseconds + "ms" ) ;
+            
+            /*
+            for ( int i = 0; i < noDuplicates.Length; i ++ )
+            {
+                Debug.Log ( "no duplicates: " + noDuplicates [i] ) ;
+            }
+            */
+
+            
+            stopwatch.Restart () ;
+
+        
+            withDuplicates.Sort();
+    Debug.Log ( "sort A " + stopwatch.ElapsedMilliseconds + "ms" ) ;
+            stopwatch.Restart () ;
+
+            int uniqueCount = withDuplicates.Unique () ;
+        
+    Debug.Log ( "multi hashmap unique A " + stopwatch.ElapsedMilliseconds + "ms" ) ;
+    Debug.Log ( "uniqueCount " + uniqueCount ) ;
+            stopwatch.Restart () ;
+            new Sort ( )
+            {
+                withDuplicates = withDuplicates2
+            }.Schedule ().Complete () ;
+        
+    Debug.Log ( "sort job burst B " + stopwatch.ElapsedMilliseconds + "ms" ) ;
+            stopwatch.Restart () ;
+
+            NativeArray <int> na_i = new NativeArray<int> (3, Allocator.TempJob )  ;
+            new Unique ( )
+            {
+                i = na_i,
+                withDuplicates = withDuplicates2
+            }.Schedule ().Complete () ;
+        
+            
+            uniqueCount = na_i [0] ;
+        
+    Debug.Log ( "multi hashmap unique job burst B " + stopwatch.ElapsedMilliseconds + "ms" ) ;
+    Debug.Log ( "uniqueCount " + uniqueCount ) ;
+            stopwatch.Restart () ;
+       
+            
+            Debug.Log ( "uniqueCount hashmap " + noDuplicates.Length ) ;
+
+        /*
+            for ( int i = 0; i < i_len; i ++ )
+            {
+                Debug.Log ( "B: " + withDuplicates [i] ) ;
+            }
+        */
+            withDuplicates2.Dispose () ;
+            dic2.Dispose () ;
+            withDuplicates.Dispose () ;
+            noDuplicates.Dispose () ;
+            dic.Dispose () ;
+
+            Debug.LogError ( "Stop" ) ;
+
+        }
+
+        
+        
+        protected override void OnUpdate ( )
+        {
+
+            /*
+            stopwatch.Restart () ;
+            stopwatch.Start () ;
+
+            int i_len = 10000 ;
+
+            NativeMultiHashMap <int,int> dic = new NativeMultiHashMap <int, int> ( i_len, Allocator.Temp ) ;
+            NativeMultiHashMap <int,int> dic2 = new NativeMultiHashMap <int, int> ( i_len, Allocator.Temp ) ;
+
+            for ( int i = 0; i < i_len; i ++ )
+            {
+                var v2 = new Vector2Int ( Random.Range ( 10, -10 ), Random.Range ( -1000, 1000 ) ) ;
                 dic.Add ( v2.x, v2.y ) ;
                 dic2.Add ( v2.x, v2.y ) ;
     // Debug.Log ( "v2: " + v2 ) ;
@@ -42,25 +145,27 @@ namespace Antypodish.ECS.Tests
             var withDuplicates2 = dic2.GetKeyArray ( Allocator.TempJob ) ;
         
     Debug.Log ( "get keys " + stopwatch.ElapsedMilliseconds ) ;
-            /*
-            for ( int i = 0; i < 1000; i ++ )
-            {
-                Debug.Log ( "A: " + withDuplicates [i] ) ;
-            }
-            */
+            
+            
+            //for ( int i = 0; i < i_len; i ++ )
+            //{
+            //    Debug.Log ( "A: " + withDuplicates [i] ) ;
+            //}
+            
+            
 
         
             withDuplicates.Sort();
-    Debug.Log ( "e1 sort A " + stopwatch.ElapsedMilliseconds ) ;
+    Debug.Log ( "sort " + stopwatch.ElapsedMilliseconds ) ;
             int uniqueCount = withDuplicates.Unique () ;
         
-    Debug.Log ( "e2 unique A " + stopwatch.ElapsedMilliseconds ) ;
+    Debug.Log ( "unique " + stopwatch.ElapsedMilliseconds ) ;
             new Sort ( )
             {
                 withDuplicates = withDuplicates2
             }.Schedule ().Complete () ;
         
-    Debug.Log ( "e1 sort B " + stopwatch.ElapsedMilliseconds ) ;
+    Debug.Log ( "sort job burst " + stopwatch.ElapsedMilliseconds ) ;
         
             NativeArray <int> na_i = new NativeArray<int> (3, Allocator.TempJob )  ;
             new Unique ( )
@@ -71,22 +176,24 @@ namespace Antypodish.ECS.Tests
         
             // uniqueCount = withDuplicates2.Unique () ;
         
-    Debug.Log ( "e2 unique B " + stopwatch.ElapsedMilliseconds ) ;
+    Debug.Log ( "unique job burst " + stopwatch.ElapsedMilliseconds ) ;
 
     Debug.Log ( "uniqueCount " + uniqueCount ) ;
        
         
-            for ( int i = 0; i < uniqueCount; i ++ )
-            {
-                Debug.Log ( "B: " + withDuplicates [i] ) ;
-            }
+            //for ( int i = 0; i < i_len; i ++ )
+            //{
+            //    Debug.Log ( "B: " + withDuplicates [i] ) ;
+            //}
         
             withDuplicates2.Dispose () ;
             dic2.Dispose () ;
             withDuplicates.Dispose () ;
             dic.Dispose () ;
-
+            */
         }
+
+
 
         [BurstCompile]
         struct Sort : IJob
@@ -111,10 +218,6 @@ namespace Antypodish.ECS.Tests
 
     
 
-
-        protected override void OnUpdate ( )
-        {
-        }
 
     }
 
